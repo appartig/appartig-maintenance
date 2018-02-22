a20caa5b768f48902b1ed9ab0cdea4d8c15a3641
Jakob Vesely <jakob@appartig.at>
2018-02-22 10:48:31 +0100

    Init Version

---
+++ appartig_maintenance.php
@@ -0,0 +1,34 @@
+<?php
+	
+	/*
+		Plugin Name: AppArtig Maintenance
+		Description: Simple Maintenance Page for not logged in Users
+		Version:     1.0.0
+		Author:      AppArtig e.U.
+		Author URI:  https://www.appartig.at
+		License:     APPARTIG/AGB
+		License URI: https://www.appartig.at/agb
+		Text Domain: aamt
+	*/
+
+	/******************************************************
+	** Main
+	******************************************************/
+
+	defined('ABSPATH') || exit('');
+	
+	function aamt_is_valid_page() {
+		return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+	}
+
+	if(!is_admin() && !aamt_is_valid_page()){  
+		
+		$aamt_title = get_bloginfo('name');
+		$aamt_URL =  plugin_dir_url(__FILE__);
+		
+		require_once('template.html');
+		
+		die();
+	} 
+
+?>
