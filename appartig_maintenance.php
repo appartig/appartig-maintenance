fe1c2d0fa276d6a77f52439ae86246fdee2fd4d2
Jakob Vesely <jakob@appartig.at>
2020-02-19 10:22:34 +0100

    Refactor and remove Text

--- appartig_maintenance.php
+++ appartig_maintenance.php
@@ -3,7 +3,7 @@
 	/*
 		Plugin Name: AppArtig Maintenance
 		Description: Simple Maintenance Page for not logged in Users
-		Version:     1.0.0
+		Version:     1.0.1
 		Author:      AppArtig e.U.
 		Author URI:  https://www.appartig.at
 		License:     APPARTIG/AGB
@@ -21,19 +21,14 @@
 		return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
 	}
 
-	function aamt_do(){
+	if(!is_admin() && !aamt_is_valid_page()){  
 		
-		if(!is_user_logged_in() && $GLOBALS['pagenow'] != 'wp-login.php' && !aamt_is_valid_page()){  
-
-			$aamt_title = get_bloginfo('name');
-			$aamt_URL =  plugin_dir_url(__FILE__);
-
-			require_once('template.html');
-
-			die();
-		} 
-	}
-
-	add_action('init', 'aamt_do');
+		$aamt_title = get_bloginfo('name');
+		$aamt_URL =  plugin_dir_url(__FILE__);
+		
+		require_once('template.html');
+		
+		die();
+	} 
 
 ?>
